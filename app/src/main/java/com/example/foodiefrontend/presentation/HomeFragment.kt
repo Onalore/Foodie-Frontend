5a062977fd47934f70f71b5dea870e2f5e0d8728
Diego Mingonette <diego_mingonette@hotmail.com>
2024-05-15 21:55:32 -0300

    ""

--- app/src/main/java/com/example/foodiefrontend/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/foodiefrontend/presentation/HomeFragment.kt
@@ -5,16 +5,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.foodiefrontend.AppColors
 
 // ViewModel para manejar la lógica de negocio si es necesario
 class HomeViewModel : ViewModel() {
@@ -36,14 +43,25 @@ class HomeFragment : Fragment() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Hola $username")
+            Text(text = "Hola $username", fontSize = 30.sp, textAlign = TextAlign.Left)
+
             // Agrega aquí más elementos de interfaz de usuario si es necesario
-            Text(text = "Que vas a comer hoy?")
-            Button(onClick = { /*TODO*/ }) {
-                Text("Sugerir recetas en base a ingredientes que tengo")
+            Text(text = "Que vas a comer hoy?", fontSize = 20.sp)
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppColors.fondoBoton,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+
+                ) {
+                Text("Sugerir recetas en base a ingredientes que tengo",
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.Center)
             }
             Button(onClick = { /*TODO*/ }) {
-                Text("Incluir cualquier ingrediente\n" + "!Hoy tengo que comprar!")
+                Text("Incluir cualquier ingrediente\n" + "!Hoy tengo que comprar!",
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.Center)
             }
         }
     }
