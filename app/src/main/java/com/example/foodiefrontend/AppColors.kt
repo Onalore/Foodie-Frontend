2ab5d06ca4012b98e86e4131595594be5243b016
Diego Mingonette <diego_mingonette@hotmail.com>
2024-05-15 21:55:32 -0300

    ""

---
+++ app/src/main/java/com/example/foodiefrontend/AppColors.kt
@@ -0,0 +1,8 @@
+package com.example.foodiefrontend
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val fondoBoton = Color(0xFF4CAF50)
+
+}
--- app/src/main/java/com/example/foodiefrontend/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/foodiefrontend/presentation/HomeFragment.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.foodiefrontend.AppColors
 
 // ViewModel para manejar la lógica de negocio si es necesario
 class HomeViewModel : ViewModel() {
@@ -39,7 +43,13 @@ class HomeFragment : Fragment() {
             Text(text = "Hola $username")
             // Agrega aquí más elementos de interfaz de usuario si es necesario
             Text(text = "Que vas a comer hoy?")
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppColors.fondoBoton,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+
+                ) {
                 Text("Sugerir recetas en base a ingredientes que tengo")
             }
             Button(onClick = { /*TODO*/ }) {
