f48289d9df4a54ef60ac65b1e275ebbf9f3c41a2
mlarotonda <mauricio.larotonda@fravega.com.ar>
2024-06-14 19:39:08 -0300

    Logeo de errores

--- app/src/main/java/com/example/foodiefrontend/presentation/ui/screens/suggestedRecipes/SuggestedRecipesViewModel.kt
+++ app/src/main/java/com/example/foodiefrontend/presentation/ui/screens/suggestedRecipes/SuggestedRecipesViewModel.kt
@@ -1,5 +1,4 @@
-package com.example.foodiefrontend.presentation.ui
-
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,25 +8,28 @@ import com.example.foodiefrontend.service.RecipesApi
 import kotlinx.coroutines.launch
 import retrofit2.awaitResponse
 
-class RecipesResultViewModel : ViewModel() {
-    private val _recipes = MutableLiveData<List<Recipe>?>()
-    val recipes: MutableLiveData<List<Recipe>?> get() = _recipes
+class SuggestedRecipesViewModel : ViewModel() {
+
+    private val _recipes = MutableLiveData<List<Recipe>>()
+    val recipes: LiveData<List<Recipe>> get() = _recipes
 
     private val _error = MutableLiveData<String>()
     val error: LiveData<String> get() = _error
 
-    fun searchRecipes() {
+    fun fetchRecipes() {
         viewModelScope.launch {
             try {
+                Log.d("SuggestedRecipesViewModel", "fetchRecipes called")
                 val response = RecipesApi.create().getRecipes().awaitResponse()
                 if (response.isSuccessful) {
-                    val recipesResponse = response.body()
-                    val recipesList = recipesResponse?.recipes ?: emptyList()
+                    val recipesList = response.body() ?: emptyList()
                     _recipes.postValue(recipesList)
                 } else {
+                    Log.d("SuggestedRecipesViewModel", "Error Recipes fetched: ${response.message()}")
                     _error.postValue("No se pudieron recuperar las recetas: ${response.message()}")
                 }
             } catch (e: Exception) {
+                Log.d("SuggestedRecipesViewModel", "Catch Recipes fetched: $e")
                 _error.postValue("Error de excepción: ${e.message}")
             }
         }
