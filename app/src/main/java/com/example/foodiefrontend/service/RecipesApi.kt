b70aaf87638c6f5d8169fd196d187cda41ee3925
mlarotonda <mauricio.larotonda@fravega.com.ar>
2024-06-14 19:38:43 -0300

    Api getRecipes

--- app/src/main/java/com/example/foodiefrontend/service/RecipesApi.kt
+++ app/src/main/java/com/example/foodiefrontend/service/RecipesApi.kt
@@ -1,15 +1,25 @@
 package com.example.foodiefrontend.service
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RecipesApi {
 
-    private val BASE_URL = "http://localhost:8080/api/gemini"
+    //En emulador 10.0.2.2 = localhost
+    private val BASE_URL = "http://10.0.2.2:8080/api/gemini/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build()
+        )
         .build()
 
     fun create(): RecipesService {
--- app/src/main/java/com/example/foodiefrontend/service/RecipesService.kt
+++ app/src/main/java/com/example/foodiefrontend/service/RecipesService.kt
@@ -1,10 +1,10 @@
 package com.example.foodiefrontend.service
 
-import com.example.foodiefrontend.data.RecipesResponse
+import com.example.foodiefrontend.data.Recipe
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface RecipesService {
-    @GET("/?q=carne%20papas%20morron%20cebolla")
-    fun getRecipes(): Call<RecipesResponse>
+    @GET("?q=carne%20papas%20morron%20cebolla")
+    fun getRecipes(): Call<List<Recipe>>
 }
