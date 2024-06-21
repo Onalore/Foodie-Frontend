48aea30c7d50515d7562bf6f32ea1e2e1e32b2cc
Mauropestarino <pestarino35@gmail.com>
2024-06-21 19:44:28 -0300

    Register conectado con back

--- app/src/main/java/com/example/foodiefrontend/data/Persona.kt
+++ app/src/main/java/com/example/foodiefrontend/data/Persona.kt
@@ -4,5 +4,5 @@ data class Persona(
     val nombre: String?,
     val apellido: String?,
     val edad: Int?,
-    val restricciones: List<String>?
+    val restricciones: List<String> = emptyList()
 )
