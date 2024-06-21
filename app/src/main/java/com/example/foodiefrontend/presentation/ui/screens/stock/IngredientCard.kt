65e893d3e3faaccf876ac2b1282601d1376e2f05
Mauropestarino <pestarino35@gmail.com>
2024-06-21 14:02:22 -0300

    ingredientcard

--- app/src/main/java/com/example/foodiefrontend/presentation/ui/screens/stock/IngredientCard.kt
+++ app/src/main/java/com/example/foodiefrontend/presentation/ui/screens/stock/IngredientCard.kt
@@ -1,35 +1,25 @@
 package com.example.foodiefrontend.presentation.ui.screens.stock
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import com.example.foodiefrontend.R
 import com.example.foodiefrontend.data.Ingredient
 import com.example.foodiefrontend.data.SampleData
 import com.example.foodiefrontend.presentation.theme.FoodieFrontendTheme
@@ -62,15 +52,15 @@ fun IngredientCard(
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = ingredient.name,
+                text = ingredient.id,
                 style = MaterialTheme.typography.subtitle1,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
             Spacer(modifier = Modifier.height(8.dp))
             IngredientQuantity(
-                quantity = ingredient.quantity,
-                unit = ingredient.unit,
+                quantity = ingredient.cantidad,
+                unit = ingredient.unidad,
                 onDecrement = { /*TODO*/ },
                 onIncrement = {}
             )
