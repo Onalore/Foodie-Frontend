6b7b68a576c204ad49e3aaf7d10e6706bd670197
Mauropestarino <pestarino35@gmail.com>
2024-07-01 17:01:04 -0300

    modifyRestrictions

--- app/src/main/java/com/example/foodiefrontend/presentation/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/foodiefrontend/presentation/ui/screens/profile/ProfileScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.foodiefrontend.R
 import com.example.foodiefrontend.navigation.AppScreens
 import com.example.foodiefrontend.presentation.ui.components.Title
